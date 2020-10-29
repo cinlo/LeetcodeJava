3fdde38646bbd64a0d47dbb4969e46891cf293bf
Cinlo <46728318+cinlo@users.noreply.github.com>
2020-10-28 17:12:51 -0700

    updates

---
+++ src/Solution/Solution148.java
@@ -0,0 +1,12 @@
+package Solution;
+
+import Structure.ListNode;
+
+public class Solution148 {
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+
+
+
+    }
+}
