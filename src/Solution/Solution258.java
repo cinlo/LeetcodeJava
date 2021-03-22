117778c630562d143d175857c0e833e69e8791d4
Cinlo <46728318+cinlo@users.noreply.github.com>
2021-03-22 13:49:58 -0700

    updates

---
+++ src/Solution/Solution258.java
@@ -0,0 +1,15 @@
+package Solution;
+
+public class Solution258 {
+    public int addDigits(int num) {
+        if (num == 0){
+            return 0;
+        }
+        if (num % 9 == 0){
+            return 9;
+        }
+        else {
+            return num % 9;
+        }
+    }
+}
